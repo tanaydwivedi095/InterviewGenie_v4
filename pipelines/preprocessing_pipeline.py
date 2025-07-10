6bc730b268bbe4bcaafe493753bd797a7c2dd2f1
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2025-07-10 15:17:04 +0530

    Update preprocessing_pipeline.py

--- pipelines/preprocessing_pipeline.py
+++ pipelines/preprocessing_pipeline.py
@@ -2,10 +2,13 @@ from components.preprocessing import get_resume, get_job_description, get_featur
 from tqdm import tqdm
 
 def preprocessing_pipeline(resume_path=r"dummy_data\resume.pdf", job_description_path=r"dummy_data\job_description.pdf"):
-    resume = get_resume(resume_path)
-    job_description = get_job_description(job_description_path)
-    features = get_features(resume=resume, job_description=job_description)
-    return features
+    try:
+        resume = get_resume(resume_path)
+        job_description = get_job_description(job_description_path)
+        features = get_features(resume=resume, job_description=job_description)
+        return features
+    except Exception as e:
+        raise CustomException(e)
 
 if __name__ == "__main__":
     print(preprocessing_pipeline())
