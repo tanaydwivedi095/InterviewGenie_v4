9f2a614064a1dbf5a9ddcc62eca4d601ccb39650
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2025-07-10 15:13:54 +0530

    Create postprocessing_simulation.py

---
+++ pipelines/postprocessing_simulation.py
@@ -0,0 +1,11 @@
+from helpers.CustomException import CustomException
+from components.postprocessing import get_conversation, split_conversation, get_report_per_question, create_report_pdf
+
+def postprocessing_pipeline():
+    try:
+        conversation = get_conversation()
+        questions = split_conversation(conversation)
+        report_dict = get_report_per_question(questions)
+        create_report_pdf(report_dict)
+    except Exception as e:
+        CustomException(e)
