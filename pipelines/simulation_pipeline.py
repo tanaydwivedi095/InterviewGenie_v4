d600ec30ef3213e6286629e5324d26547ec4709a
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2025-07-10 15:15:34 +0530

    Update simulation_pipeline.py

--- pipelines/simulation_pipeline.py
+++ pipelines/simulation_pipeline.py
@@ -3,20 +3,26 @@ from helpers.utils import load_llm
 from helpers.CustomException import CustomException
 
 def end_simulation():
-    prompt = f"""
+    prompt = """
     Generate a very small script thanking the interviewee and ending the interview process
     """
-    llm = load_llm()
-    end_script = llm.invoke(prompt)
-    return end_script
+    try:
+        llm = load_llm()
+        end_script = llm.invoke(prompt)
+        return end_script
+    except Exception as e:
+        raise CustomException(e)
     
 def simulation_pipeline():
-    features = get_features()
-    for question_idx in range(0,5):
-        previous_conversation = get_conversation()
-        master_prompt = create_prompt(previous_conversation, features)
-        question = generate_question(master_prompt)
-        answer = get_answer()
-        save_conversation(previous_conversation, question, answer)
-    print(end_simulation())
+    try:
+        features = get_features()
+        for question_idx in range(0,5):
+            previous_conversation = get_conversation()
+            master_prompt = create_prompt(previous_conversation, features)
+            question = generate_question(master_prompt)
+            answer = get_answer()
+            save_conversation(previous_conversation, question, answer)
+        print(end_simulation())
+    except Exception as e:
+        raise CustomException(e)
     
